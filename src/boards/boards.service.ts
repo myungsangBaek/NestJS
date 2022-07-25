3f54c94c35eb28ad936efef297ef1371bb844f29
myungsangBaek <bms6168@naver.com>
2022-07-25 17:46:29 +0900

    [FEATURE] Generate create board

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
-import { Board } from './boards.model';
+import { v1 as uuid } from 'uuid';
+import { Board, BoardStatus } from './boards.model';
 
 @Injectable()
 export class BoardsService {
@@ -7,5 +8,17 @@ export class BoardsService {
 
   getAllBoards(): Board[] {
     return this.boards;
+  }
+
+  createBoard(title: string, description: string) {
+    const board: Board = {
+      id: uuid(),
+      title,
+      description,
+      status: BoardStatus.PUBLIC,
+    };
+
+    this.boards.push(board);
+    return board;
   }
 }
